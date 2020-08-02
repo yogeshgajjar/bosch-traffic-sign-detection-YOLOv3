f4e94606dee3a32cd6bb74ceb0b58838ed28129a
Yogesh Gajjar <yogeshgajjar75@gmail.com>
2020-08-01 18:29:03 -0700

    new files

---
+++ utils/image_list_generator.py
@@ -0,0 +1,44 @@
+"""
+Copyright © 2020 Yogesh Gajjar. All rights reserved.
+"""
+
+import os
+import sys
+
+
+def combine_images(img_path):
+	"""
+	This function reads the .png image file and converts into a form requried
+	for training. Later this can be converted to test and train .txt files.
+
+	args:
+		1. img_path : The image file path
+
+	"""
+
+	image_files = []
+	for r,d,f in os.walk(img_path):
+		#r = root, d = directories, f = image_files
+		for file in f:
+			if '.png' in file:
+				image_files.append(os.path.join(r, file)) #Gets the whole file img_path
+
+	file_num = len(image_files)
+	print("Length of the image_files: ", file_num)
+
+	if not open('traffic_lights_images.txt','w'):
+		os.makefile('traffic_lights_images.txt')
+
+	labels = open('traffic_lights_images.txt','w')
+
+	for xml in image_files:
+		labels.write(xml + '\n')
+
+	labels.close()
+
+def main():
+	image_path = sys.argv[1] #xml files path
+	combine_labels(image_path)
+
+if __name__ == '__main__':
+    main()
