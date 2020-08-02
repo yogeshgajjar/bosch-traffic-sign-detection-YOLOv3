511dbec2b55d4383ccc744b9af49c186690f32b0
Yogesh Gajjar <yogeshgajjar75@gmail.com>
2020-08-01 18:54:22 -0700

    changes

--- utils/train_test_split.py
+++ utils/train_test_split.py
@@ -1,22 +1,41 @@
+"""
+Copyright © 2020 Yogesh Gajjar. All rights reserved.
+"""
+
 import glob, os
 import numpy as np
 from sklearn.model_selection import train_test_split
 import sys
 
-image_paths_file = sys.argv[1] #traffic_lights/traffic_lights.txt
 
-# Percentage of images to be used for the test set (float between 0-1)
-percentage_test = float(sys.argv[2]);
+def split_test_train(image_paths_file, percentage):
+	"""
+	This function splits the image .txt file to test and train .txt file. The
+	ratio depends on the percentage selected by the user.
 
-img_paths = []
-img_paths = open(image_paths_file).read().strip().split()
+	args:
+		1. image_paths_file 	: The image .txt file path
+		2. percentage	 		: Value in the range (0,1)
 
-X_train, X_test= train_test_split(img_paths, test_size=percentage_test, random_state=31)
+	"""
 
-with open('train.txt', 'a') as train_file:
-	for train in X_train:
-		train_file.write(train + '\n')
+	img_paths = []
+	img_paths = open(image_paths_file).read().strip().split()
 
-with open('test.txt', 'a') as test_file:
-	for test in X_test:
-		test_file.write(test + '\n')
+	X_train, X_test= train_test_split(img_paths, test_size=percentage, random_state=31)
+
+	with open('train.txt', 'a') as train_file:
+		for train in X_train:
+			train_file.write(train + '\n')
+
+	with open('test.txt', 'a') as test_file:
+		for test in X_test:
+			test_file.write(test + '\n')
+
+def main():
+	image_paths_file = sys.argv[1]
+	percentage_test = float(sys.argv[2]);
+
+
+if __name__ == '__main__':
+    main()
